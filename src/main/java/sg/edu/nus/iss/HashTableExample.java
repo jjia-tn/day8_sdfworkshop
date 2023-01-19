056f52f543a8a0859fcd697203d5374f61fd301d
jjia-tn <jjia.tn@gmail.com>
2023-01-19 11:32:06 +0800

    HashMapExample

---
+++ src/main/java/sg/edu/nus/iss/HashTableExample.java
@@ -0,0 +1,43 @@
+package sg.edu.nus.iss;
+
+import java.io.Console;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashTableExample {
+    public void example() {
+        Map<String, Integer> phones = new Hashtable<>();
+
+        phones.put("iPhone", 10);
+        phones.put("Huawei", 2);
+        phones.put("Oppo", 4);
+        phones.put("Samsung", 20);
+
+        System.out.println("Total phone types: " + phones.size());
+        
+        for (String key: phones.keySet()) {
+            System.out.println(key + " - " + phones.get(key));
+        }
+
+        String searchString = "";
+        Console con = System.console();
+
+        while (!searchString.equals("quit")) {
+            searchString = con.readLine("Enter searchString: ");
+
+            if (phones.containsKey(searchString)) {
+                System.out.println(searchString + " - " + phones.get(searchString));
+            } else {
+                System.out.println("Item not found");
+            }
+        }
+
+        // remove all the Map data
+        phones.clear();
+
+        // check the size of map after clear()
+        System.out.println("Total phone types: " + phones.size());
+    }
+    
+}
+
